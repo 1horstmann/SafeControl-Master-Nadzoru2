322c38c4b07e009d92601c7ee4bee3b2b766589a
Guilherme Tsubahara Horstmann <guilhermehorstmann1@gmail.com>
2021-08-09 08:49:06 -0300

    Apagado comentários

--- Algorithm.py
+++ Algorithm.py
@@ -20,18 +20,19 @@ import StringsFunctions
 #   Guilherme Tsubahara Horstmann                           #
 #***********************************************************#
 
-StringsFunctions.ConsideredStrings_Publish() 
-StringsFunctions.IsDiag()
-StringsFunctions.IsDiag_Publish()
-StringsFunctions.IsSafeDiag()
-StringsFunctions.IsSafeDiag_Publish()
-StringsFunctions.IsPred()
-StringsFunctions.IsPred_Publish()
-StringsFunctions.IsSafeControlByDiag() 
-StringsFunctions.IsSafeControlByDiag_Publish()
-StringsFunctions.IsSafeControlByProg()
-StringsFunctions.IsSafeControlByProg_Publish()
-StringsFunctions.IsSafeControlByDiagAndProg()
-StringsFunctions.IsSafeControlByDiagAndProg_Publish()
-StringsFunctions.IsSafeControlByDiagAndProg_SelfPublish()
+# StringsFunctions.ConsideredStrings_Publish() 
+# # StringsFunctions.IsDiag()
+# StringsFunctions.IsDiag_Publish()
+# StringsFunctions.IsSafeDiag()
+# StringsFunctions.IsSafeDiag_Publish()
+# StringsFunctions.IsPred()
+# StringsFunctions.IsPred_Publish()
+# StringsFunctions.IsSafeControlByDiag() 
+# StringsFunctions.IsSafeControlByDiag_Publish()
+# StringsFunctions.IsSafeControlByProg()
+# StringsFunctions.IsSafeControlByProg_Publish()
+# StringsFunctions.IsSafeControlByDiagAndProg()
+# StringsFunctions.IsSafeControlByDiagAndProg_Publish()
+# StringsFunctions.IsSafeControlByDiagAndProg_SelfPublish()
+
 
